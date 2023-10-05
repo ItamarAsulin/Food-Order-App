6bf487d4b2e4bdbdee933bf7e807df221e064511
Ubuntu <ubuntu@ip-172-31-18-239.eu-central-1.compute.internal>
2023-10-05 11:12:27 +0000

    updated adding to cart animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon/CartIcon";
 import CartContext from "../../store/CartContext";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlited] = useState(false);
   const cartCtx = useContext(CartContext);
   let totalItems = 0;
   for (const item of cartCtx.itemsList) {
     totalItems += Number(item.amount);
   }
 
+  let cartIconClass = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  const { itemsList } = cartCtx;
+
+  useEffect(() => {
+    if (itemsList.length === 0) {
+      console.log("here");
+
+      return;
+    }
+    setBtnIsHighlited(true);
+    setTimeout(() => {
+      setBtnIsHighlited(false);
+    }, 300);
+  }, [itemsList]);
+
   return (
-    <button className={classes.button} onClick={cartCtx.showCart}>
+    <button className={`${cartIconClass}`} onClick={cartCtx.showCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
